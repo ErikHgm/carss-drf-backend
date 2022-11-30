12809a83ef0168db3ef7b92ea9c4c67de1a57d70
Erik <erikhgm@gmail.com>
2022-11-30 16:05:50 +0000

    feat: add profiledetails view and urls

--- profiles/views.py
+++ profiles/views.py
@@ -6,3 +6,8 @@ from .serializers import ProfileSerializer
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+
+
+class ProfileDetails(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
