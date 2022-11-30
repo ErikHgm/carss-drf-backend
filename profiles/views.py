7053b0fad00a780cba96db76efbe372666e35fd1
Erik <erikhgm@gmail.com>
2022-11-30 16:53:31 +0000

    feat: add custom permissions

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Profile
 from .serializers import ProfileSerializer
+from carss_drf_backend.permissions import IsOwnerOrReadOnly
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
 
 
 class ProfileList(generics.ListAPIView):
@@ -10,4 +18,5 @@ class ProfileList(generics.ListAPIView):
 
 class ProfileDetails(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
