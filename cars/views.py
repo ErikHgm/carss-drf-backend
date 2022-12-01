563067c739c4e4264c24586b2ba3ce3effb89c58
Erik <erikhgm@gmail.com>
2022-12-01 15:01:49 +0000

    feat: add cardetail view and urls

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,11 @@
 from rest_framework import generics, permissions
+from carss_drf_backend.permissions import IsOwnerOrReadOnly
 from .models import Car
 from .serializers import CarSerializer
 
 
 class CarList(generics.ListCreateAPIView):
-    """ 
+    """
     List car posts and create post for logged in users.
     Perform_create method ties a post to the logged in user.
     """
@@ -15,3 +16,13 @@ class CarList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class CarDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Detailed view of a car posting.
+    Update or delete a posting for owner of the post.
+    """
+    serializer_class = CarSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Car.objects.all()
