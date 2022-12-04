9f5cca7e8ad209b12dccd6f58784242ee2089789
Erik <erikhgm@gmail.com>
2022-12-04 10:13:25 +0000

    feat: add follower listview, urls and serializers and model migrations

--- carss_drf_backend/settings.py
+++ carss_drf_backend/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'profiles',
     'cars',
     'saved',
+    'followers',
 ]
 
 MIDDLEWARE = [
--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all instances of a user
+    following another user'.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
