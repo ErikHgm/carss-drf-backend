c5736fbd2b2e457d71d0bb3ce2281b5bad1fa5ac
Erik <erikhgm@gmail.com>
2022-12-05 11:01:54 +0000

    feat: add save_id field to car serializer

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Car
+from saved.models import Save
 
 
 class CarSerializer(serializers.ModelSerializer):
@@ -8,10 +9,20 @@ class CarSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    save_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_save_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            save = Save.objects.filter(
+                owner=user, car=obj
+            ).first()
+            return save.id if save else None
+        return None
 
     class Meta:
         model = Car
