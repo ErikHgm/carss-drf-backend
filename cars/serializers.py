e5856202ad678cdad1a8cd4a7a299c59c1371959
Erik <erikhgm@gmail.com>
2022-12-18 16:22:52 +0000

    fix: add image validator and profile_image field

--- cars/serializers.py
+++ cars/serializers.py
@@ -9,7 +9,18 @@ class CarSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     save_id = serializers.SerializerMethodField()
+
+    def validate_image(self, value):
+        if value.size > 4096 * 4096 * 2:
+            raise serializers.ValidationError('Image size larger than 2mb')
+        if value.image.width > 4096:
+            raise serializers.ValidationError('Image width to large')
+        if value.image.height > 4096:
+            raise serializers.ValidationError('Image height to large')
+
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
