b564ce9a73baacf94971bdcd424e261e2f86fd8c
Erik <erikhgm@gmail.com>
2022-12-19 11:38:12 +0000

    build: update settings.py

--- carss_drf_backend/settings.py
+++ carss_drf_backend/settings.py
@@ -26,7 +26,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
